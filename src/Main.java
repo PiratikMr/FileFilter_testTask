7f738c0ea44ef72ce1dbcb33e6491afcd3b26646
pirat <hitskov75@gmail.com>
2024-07-14 20:37:52 +0500

    Updated process potential error #6

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.print(Filter.filter(args));
-        System.out.print(Filter.filter(new String[]{"-f","-o","C:\\Users\\Александр\\Desktop\\folder", "C:\\Users\\Александр\\Desktop\\testIn1.txt"}));
+        System.out.print(Filter.filter(args));
     }
 }
